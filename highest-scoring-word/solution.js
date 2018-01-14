550f6843ab7bf02480feee5bca94eeba6915e7e0
stariel <ariel.altaras@gmail.com>
2018-01-14 14:25:30 -0800

    finished solution and added whiteboarding picture

--- highest-scoring-word/solution.js
+++ highest-scoring-word/solution.js
@@ -1,19 +1,19 @@
 function high(x){
-  var value = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var score = [];
-  var words = x.split(' ');
-    
-  function scoreWord () {
+    var value = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var score = [];
+    var words = x.split(' ');
     var tally = 0;
-    for (j = 0; j < words.length; j++) {
+    var letterscore = 0;
+    for (var j = 0; j < words.length; j++) {
       var wordArr = words[j].split('');
-      for (i=0 ; i<wordArr.length; i++) {
-        var letterscore = value.indexOf(wordArr[i]);
-        var wordScore = letterscore + 1 + tally;
-        i++;
-        score.push(wordScore);}
+      for (var i = 0 ; i < wordArr.length; i++) {
+        letterscore = value.indexOf(wordArr[i]);
+        tally = tally + letterscore + 1;
+        letterscore = 0;
+      }
+        score.push(tally);
+        tally = 0;
     }
+    var highScore = score.indexOf(Math.max(...score));
+    return words[highScore];
   }
-  var highScore = score.indexOf(Math.max(score));
-  return words[highScore];
-}
